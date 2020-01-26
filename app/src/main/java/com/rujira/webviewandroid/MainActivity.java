89c286814fd33e3b5861c1ce30358187e3f72c37
Rujira Kongsomran <biwberry@outlook.com>
2020-01-26 16:30:07 +0700

    Initial webView in MainActivity

--- app/src/main/java/com/rujira/webviewandroid/MainActivity.java
+++ app/src/main/java/com/rujira/webviewandroid/MainActivity.java
@@ -3,12 +3,25 @@ package com.rujira.webviewandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+
     }
 }
