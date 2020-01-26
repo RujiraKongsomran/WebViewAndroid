9667fc62f9870e54a21bf990f38fef5768ca1e3d
Rujira Kongsomran <biwberry@outlook.com>
2020-01-26 16:41:21 +0700

    Add code in case webView go back

--- app/src/main/java/com/rujira/webviewandroid/MainActivity.java
+++ app/src/main/java/com/rujira/webviewandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.rujira.webviewandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,7 +22,19 @@ public class MainActivity extends AppCompatActivity {
     private void initInstances() {
         webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://www.google.com");
+        webView.loadUrl("https://www.google.com");
 
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
